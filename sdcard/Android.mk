db37d303a39e9be9651d80dff6c845364fe50926
Daniel Micay <danielmicay@gmail.com>
2016-05-05 16:01:49 -0400

    enable integer sanitizer for sdcard service
    The previous false positive fix (df9c4a01) is enough to pass tests, and
    it doesn't appear that there are any remaining issues.
    
    Change-Id: Ib9812f1201ff0cd2ae8c8371737754fc328765b5

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -10,4 +10,7 @@ LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
 LOCAL_SHARED_LIBRARIES := libcutils libpackagelistparser
 
+LOCAL_SANITIZE := integer
+LOCAL_CLANG := true
+
 include $(BUILD_EXECUTABLE)

b07ab4dd093b8821e9ebcdb282825afabc561e1d
Jorge Lucangeli Obes <jorgelo@google.com>
2016-07-12 17:05:32 -0400

    Use C++ logging in sdcard.cpp.
    This gets rid of a bunch of "strerror(errno)" bits.
    Will convert fuse.cpp after
    https://android-review.googlesource.com/#/c/247780 lands.
    
    Bug: 30110940
    
    Change-Id: Iacefe5b6519b217ed687c709763fe5827b3b0b59

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -9,7 +9,7 @@ endif
 LOCAL_SRC_FILES := sdcard.cpp fuse.cpp
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
-LOCAL_SHARED_LIBRARIES := libcutils libpackagelistparser
+LOCAL_SHARED_LIBRARIES := libbase liblog libcutils libpackagelistparser
 
 LOCAL_SANITIZE := integer
 LOCAL_CLANG := true

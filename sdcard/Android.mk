6a58d73ebd5253c9d5843a625e96a810bf28a047
Jorge Lucangeli Obes <jorgelo@google.com>
2016-07-12 15:13:05 -0400

    Extract the FUSE implementation from the main sdcard.c file.
    sdcard.c is a *really* big file. This makes it hard to do things like
    improving priv dropping or adding more sandboxing. Extract all
    FUSE-related code to a separate unit, fuse.{h|c}, which exports only
    two functions. Convert the rest of sdcard.c to C++ as sdcard.cpp.
    fuse.c is kept as C (at least for now) since interacting with the FUSE
    API is realistically easier from C.
    
    Bug: 30110940
    
    Change-Id: I188bfdc21c184742117e07539adb09090d4d747c

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -5,7 +5,7 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
-LOCAL_SRC_FILES := sdcard.c
+LOCAL_SRC_FILES := sdcard.cpp fuse.c
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
 LOCAL_SHARED_LIBRARIES := libcutils libpackagelistparser
